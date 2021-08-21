489f1e48c473aabad42a9ab67d0762360795ba36
Tarun5311 <anumolu.tarunkumar247@gmail.com>
2021-08-21 23:59:35 +0530

    feat: Add main screen

--- portfolio/lib/screens/main/main_screen.dart
+++ portfolio/lib/screens/main/main_screen.dart
@@ -14,9 +14,7 @@ class MainScreen extends StatelessWidget {
             children: [
               Expanded(
                 flex: 2,
-                child: Container(
-                  color: Colors.white,
-                ),
+                child: sidemenu()
               ),
               Expanded(
                 flex: 7,
@@ -27,6 +25,52 @@ class MainScreen extends StatelessWidget {
             ]
           ),
         ),
+      ),
+    );
+  }
+}
+
+class sidemenu extends StatelessWidget {
+  const sidemenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1.23,
+            child: Container(
+              color: Color(0xFF242430),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Spacer(flex: 2),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage("assets/images/Profile.jpeg"),
+                  ),
+                  Spacer(),
+                  Text(
+                    'Anumolu Tarun Kumar',
+                    style: Theme.of(context).textTheme.subtitle2,
+                  ),
+                  Text(
+                    '3rd year IMtech CSE student at \n IIITBangalore',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w200,
+                      height: 1.5,
+                    )
+                  ),
+                  Spacer(flex: 2),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
